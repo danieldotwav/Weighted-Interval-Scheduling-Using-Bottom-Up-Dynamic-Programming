24b1134aaf291a1d1a0954f26b1b6833b7bc6744
Daniel Rivas <drivas8891@gmail.com>
2024-02-14 14:50:21 -0800

    Formatted output on console for enhanced readability.

--- WISmain.cpp
+++ WISmain.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <algorithm>
 #include "WISheader.h";
 using namespace std;
 
+const int INDEX_WIDTH = 15;
+const int JOB_DETAILS_WIDTH = 8;
+
 bool isValidWISParameters(int start, int finish, int profit);
 bool compareByFinishTime(const WIS& firstJob, const WIS& secondJob);
-void printWISContainer(vector<WIS> container);
+void printWISContainer(const vector<WIS> container);
+void printFormattedInputIntervals(const vector<WIS> container);
 
 int main() {
 	// You are given a list of jobs to complete and you only have one machine.
@@ -23,7 +28,7 @@ int main() {
 	// Create a container to store WIS 
 	vector<WIS> jobs;
 
-	// Populate WIS objects "intervals" number of times
+	// Populate WIS container
 	cout << "Enter Start time, Finish time, and Weight separated by a space:\nSi Fi Wi\n";
 
 	int start, finish, profit;
@@ -41,11 +46,13 @@ int main() {
 	sort(jobs.begin(), jobs.end(), compareByFinishTime);
 
 	// Display the results
-	printWISContainer(jobs);
+	cout << "\nSorted Input Intervals By Finishing Time:\n";
+	printFormattedInputIntervals(jobs);
 
 	return 0;
 }
 
+// Validates record data and returns the state of the values as a boolean
 bool isValidWISParameters(int start, int finish, int profit) {
 	bool isValid = false;
 	// First check for negative numbers
@@ -65,12 +72,34 @@ bool isValidWISParameters(int start, int finish, int profit) {
 	return isValid;
 }
 
+// Sorting logic for the std::sort method
 bool compareByFinishTime(const WIS& firstJob, const WIS& secondJob) {
 	return firstJob.getFinishTime() < secondJob.getFinishTime();
 }
 
 void printWISContainer(vector<WIS> container) {
+	if (!container.empty()) {
+		cout << "\nNo WIS Records To Process\n";
+	}
+	
 	for (WIS element : container) {
 		element.printFormattedInterval();
+	}
+}
+
+void printFormattedInputIntervals(const vector<WIS> container) {
+	cout << "\t" << left << setw(INDEX_WIDTH) <<
+		"Index<i>" << setw(JOB_DETAILS_WIDTH) <<
+		"S<i>" << setw(JOB_DETAILS_WIDTH) << 
+		"F<i>" << setw(JOB_DETAILS_WIDTH) <<
+		"W<i>\n";
+
+	int numWISRecords = container.size();
+	for (int i = 0; i < numWISRecords; ++i) {
+		cout << "\t" <<
+			setw(INDEX_WIDTH) << i + 1 <<
+			setw(JOB_DETAILS_WIDTH) << container[i].getStartTime() <<
+			setw(JOB_DETAILS_WIDTH) << container[i].getFinishTime() <<
+			setw(JOB_DETAILS_WIDTH) << container[i].getProfit() << endl;
 	}
 }
